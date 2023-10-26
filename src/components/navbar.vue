d1f3b7b3c0f39e8e966357311b2425f056d65375
kamilarici <kamil@testinvite.com>
2023-10-26 23:51:45 +0300

    edit

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,25 +1,36 @@
-
-
 <template>
-    <nav navbar navbar-expand-lg bg-body-tertiary>
-<div class="container">
-
-    <a class="navbar-brand" href="#">AOS sosyal </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg   bg-light" >
+    <div class="container">
+      <a class="navbar-brand" href="#">AOS sosyall </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">paykaş</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-    </ul>
-</div>
-</div>
-        
-    </nav>
-
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">paylaş</a>
+          </li>
+        </ul>
+          <div class="d-flex" >
+            <router-link to="/login">
+              <button class="btn btn-outline-success" type="submit">Giriş</button>
+            </router-link>
+          <button class="btn btn-outline-danger" type="submit">Çikiş</button>
+          </div>
+       
+      </div>
+    </div>
+  </nav>
 </template>
+<style>
+html,body{
+  background: #dad7cd;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="form-signin m-auto w-100">
+    <div class="row">
+        <div class="col-12">
+            <button class="btn btn-primary"> <i class="bi bi-google"></i> ile giriş</button>
+        </div>
+
+    </div>
+
+  </main>
+</template>
+
+<style scoped>
+.form-signin{
+    max-width: 330px;
+    padding: 15px;
+    padding-top: 140px;
+}
+</style>
