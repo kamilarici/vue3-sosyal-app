1374d57d9b896dfafe90b84964ed236c1b13118a
kamilarici <kamil@testinvite.com>
2023-10-27 22:55:35 +0300

    ..

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -14,21 +14,44 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-          <li class="nav-item">
+          <li class="nav-item" v-show="kullanici">
             <a class="nav-link active" aria-current="page" href="#">paylaş</a>
           </li>
         </ul>
           <div class="d-flex" >
             <router-link to="/login">
-              <button class="btn btn-outline-success" type="submit">Giriş</button>
+              <button class="btn btn-outline-success" type="submit" v-show="!kullanici">Giriş</button>
             </router-link>
-          <button class="btn btn-outline-danger" type="submit">Çikiş</button>
+          <button class="btn btn-outline-danger" type="submit" @click="handleCikis" v-show="kullanici">Çikiş</button>
           </div>
        
       </div>
     </div>
   </nav>
 </template>
+<script>
+import getUser from '@/composables/getUser';
+import { auth } from '../firebase/config';
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'vue-router';
+
+export default{
+  setup(){
+
+const {kullanici}=getUser()
+const router=useRouter()
+
+const handleCikis=async()=>{
+  await signOut(auth)
+  router.push({name:'Login'})
+}
+
+
+
+return {kullanici,handleCikis}
+  }
+}
+</script>
 <style>
 html,body{
   background: #dad7cd;
