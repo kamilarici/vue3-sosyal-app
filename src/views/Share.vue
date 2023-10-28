6caf4c4a1243fb2458ec90663e4738049c035336
kamilarici <kamil@testinvite.com>
2023-10-28 22:37:38 +0300

    moment

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -1,83 +1,112 @@
 <template>
-    <div class="container">
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Düşüncelerini Yaz</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Bugün Çok Mutluyum" v-model="gonderi">
-        </div>
-        <div class="mb-3 text-center">
-            <button type="button" class="btn btn-dark" @click="handleClick">Gönder</button>
-        </div>
-        <hr>
-        <div class="mb-3">
-            <ol class="list-group list-group-numbered">
-                <li v-for="g in gonderiler" :key="g.id" class="list-group-item d-flex justify-content-center align-items-start">
-                    <div class="ms-2 me-auto">
-                        <div class="fw-bold">{{g.gonderi}}</div>
-                        {{g.tarih}}
-                    </div>
-                    <span class="badge bg-primary rounded-pill">{{g.yorumlar.length}}</span>
-                    <span @click="handleDelete(g.id)" class="badge bg-danger mx-2"><i class="bi bi-x"></i></span>
-
-                </li>
-           
-                
-            </ol>
-        </div>
-      
+  <div class="container">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label"
+        >Düşüncelerini Yaz</label
+      >
+      <input
+        type="text"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="Bugün Çok Mutluyum"
+        v-model="gonderi"
+      />
     </div>
+    <div class="mb-3 text-center">
+      <button type="button" class="btn btn-dark" @click="handleClick">
+        Gönder
+      </button>
+    </div>
+    <hr />
+    <div class="mb-3">
+      <ol class="list-group list-group-numbered">
+        <li
+          v-for="g in gonderiler"
+          :key="g.id"
+          class="list-group-item d-flex justify-content-center align-items-start"
+        >
+          <div class="ms-2 me-auto">
+            <div class="fw-bold">{{ g.gonderi }}</div>
+            {{ g.tarih }}
+          </div>
+          <span class="badge bg-primary rounded-pill">{{
+            g.yorumlar.length
+          }}</span>
+          <span @click="handleDelete(g.id)" class="badge bg-danger mx-2"
+            ><i class="bi bi-x"></i
+          ></span>
+        </li>
+      </ol>
+    </div>
+  </div>
 </template>
 <script>
-import { ref,onMounted } from 'vue';
-import getUser from '@/composables/getUser'; 
-import { getDoc,collection,serverTimestamp, addDoc,onSnapshot,query,where, deleteDoc,doc} from 'firebase/firestore';
-import { db } from '@/firebase/config';
+import { ref, onMounted } from "vue";
+import getUser from "@/composables/getUser";
+import {
+  getDoc,
+  collection,
+  serverTimestamp,
+  addDoc,
+  onSnapshot,
+  query,
+  where,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { db } from "@/firebase/config";
+import moment from "moment";
 export default {
-    setup() {
-        
+  setup() {
+    // tarih formatı
+    moment.locale("tr");
+    const { kullanici } = getUser();
+    const gonderi = ref("");
+    const gonderiler = ref([]);
+    const handleClick = async () => {
+      if (kullanici.value) {
+        await addDoc(collection(db, "gonderiler"), {
+          gKullaniciAd: kullanici.value.displayName,
+          gonderi: gonderi.value,
+          tarih: serverTimestamp(),
+          yorumlar: [],
+        });
+        gonderi.value = "";
+      }
+    };
 
-        const {kullanici}=getUser()
-        const gonderi=ref('')
-        const gonderiler=ref([])
-        const handleClick=async()=>{
-            if(kullanici.value){
-                await addDoc(collection(db,'gonderiler'),{
-                    gKullaniciAd:kullanici.value.displayName,
-                    gonderi:gonderi.value,
-                    tarih:serverTimestamp(),
-                    yorumlar:[]
-                })
-                gonderi.value=''
-            }
-        }
+    const handleDelete = async (id) => {
+      // console.log(id)
+      await deleteDoc(doc(db, "gonderiler", id));
+    };
 
-const handleDelete=async(id)=>{
-    // console.log(id)
-    await deleteDoc(doc(db,"gonderiler",id))
-}
+    onMounted(() => {
+      const q = query(
+        collection(db, "gonderiler"),
+        where("gKullaniciAd", "==", kullanici.value.displayName)
+      );
 
-onMounted(()=>{
+      onSnapshot(q, (querySnapshot) => {
+        const dizi = [];
 
-    const q=query(collection(db,'gonderiler'),where('gKullaniciAd',"==",kullanici.value.displayName))
+        querySnapshot.forEach((doc) => {
+          dizi.push({
+            ...doc.data(),
+            id: doc.id,
+            tarih: moment(doc.data().tarih.toDate()).format("LLL"),
+          });
+        });
+        gonderiler.value = dizi;
+      });
+    });
 
-    onSnapshot(q,querySnapshot=>{
-        const dizi=[]
-
-        querySnapshot.forEach(doc=>{
-            dizi.push({...doc.data(),id:doc.id})
-
-        })
-        gonderiler.value=dizi;
-    } )
-})
-
-
-        return {gonderi,handleClick,gonderiler ,handleDelete}
-    }, 
-}
+    return { gonderi, handleClick, gonderiler, handleDelete };
+  },
+};
 </script>
 <style scoped>
-.container{
-    max-width: 600px;
-    padding-top: 50px;
+.container {
+  max-width: 600px;
+  padding-top: 50px;
 }
 </style>
